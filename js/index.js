c53fd9f5d2a4430932767068fbf873d918128712
CamilleM28 <Camillem2812@gmail.com>
2022-01-12 16:22:21 +0000

    fixed fault - [i] is now recognised

--- js/index.js
+++ js/index.js
@@ -1,37 +1,33 @@
-import './data.js' 
-import { dates } from './data.js'
+import "./data.js";
+import { dates } from "./data.js";
+
+const timeline = document.querySelector(".timeline");
 
 const newDiv = document.createElement("div");
-newDiv.className = 'timeline-item';
+newDiv.className = "timeline-item";
 
+const h2 = document.createElement("h2");
+h2.className = "timeline-item-title";
+h2.textContent = dates.title;
 
-for(let i = 0; i < dates.length; i++); {
-    const h2 = document.createElement("h2")
-h2.className = 'timeline-item-title'
-h2.textContent = dates.title
+const span = document.createElement("span");
+span.className = "timeline-item-date";
+span.textContent = dates.date;
 
+const text = document.createElement("text");
+text.className = "timeline-item-summary";
+text.textContent = dates.summary;
 
-const span = document.createElement("span")
-span.className = 'timeline-item-date'
-span.textContent = dates.date 
+const info = document.createElement("info");
+info.className = "timeline-item-more-info";
+info.textContent = dates.fullDescription;
 
-
-const text = document.createElement("text")
-text.className = 'timeline-item-summary'
-text.textContent = dates.summary
-
-const info = document.createElement("info")
-info.className = 'timeline-item-more-info'
-info.textContent = dates.fullDescription
-
-const timeline = document.querySelector(".timeline")
-
-newDiv.textContent = dates[3].date
-
-
-newDiv.appendChild(h2)
-newDiv.appendChild(span)
-newDiv.appendChild(text)
-newDiv.appendChild(info)
+newDiv.appendChild(h2);
+newDiv.appendChild(span);
+newDiv.appendChild(text);
+newDiv.appendChild(info);
 timeline.appendChild(newDiv);
+
+for (let i = 0; i < dates.length; i++) {
+  newDiv.textContent = dates[i].title + dates[i].summary;
 }
