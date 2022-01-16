2cf3c317b6b30326c7b1a06d62863fc4bd10e201
samkeliso <samchiota@gmail.com>
2022-01-16 12:38:05 +0000

    displayed fulltimeline in the DOM

--- js/index.js
+++ js/index.js
@@ -6,30 +6,30 @@ newDiv.className = 'timeline-item';
 
 
 for(let i = 0; i < dates.length; i++) {
+
+    const timeline = document.querySelector(".timeline");
     console.log(dates)
     console.log(i)
     const h2 = document.createElement("h2")
     h2.className = 'timeline-item-title'
     h2.textContent = dates[i].title
+    newDiv.appendChild(h2)
 
-const timeline = document.querySelector(".timeline");
 
     const span = document.createElement("span")
     span.className = 'timeline-item-date'
     span.textContent = dates[i].date 
     newDiv.appendChild(span)
 
-    const text = document.createElement("text")
+    const text = document.createElement("p")
     text.className = 'timeline-item-summary'
     text.textContent = dates[i].summary
+    newDiv.appendChild(text)
 
     const info = document.createElement("info")
     info.className = 'timeline-item-more-info'
     info.textContent = dates[i].fullDescription
-
-
-    newDiv.appendChild(h2)
-    newDiv.appendChild(text)
     newDiv.appendChild(info)
+
     timeline.appendChild(newDiv);
 }
